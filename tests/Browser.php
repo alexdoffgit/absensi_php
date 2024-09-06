c1daba710cbee54cb79235a11ab319dea9843bbc
miridium-electrode <checkerdance@yahoo.com>
2024-09-06 16:26:18 +0700

    test: browser specific code

---
+++ tests/Browser.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests;
+
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Firefox\FirefoxOptions;
+
+class Browser {
+    private static $driver;
+
+    public static function getDriver() 
+    {
+        if (self::$driver === null) {
+            $host = 'http://localhost:4444';
+    
+            $options = new FirefoxOptions();
+            $options->setOption('binary', 'C:\\Program Files\\Mozilla Firefox\\firefox.exe');
+    
+            $capabilities = DesiredCapabilities::firefox();
+            $capabilities->setCapability(FirefoxOptions::CAPABILITY, $options);
+    
+            self::$driver = RemoteWebDriver::create($host, $capabilities);
+            self::$driver->manage()->window()->maximize();    
+        }
+
+        return self::$driver;
+    }
+
+    public static function quitDriver()
+    {
+        if (self::$driver !== null) {
+            self::$driver->quit();
+            self::$driver = null;
+        }
+    }
+}
