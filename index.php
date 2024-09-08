8deb4d4cc68e8c80c074097ee4d3718306984f67
miridium-electrode <checkerdance@yahoo.com>
2024-09-08 19:10:33 +0700

    redirect index page to login

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
 <?php
-require 'vendor/autoload.php';
-
-$request = str_replace('/absensi_php', '', $_SERVER['REQUEST_URI']); // this is a hack as i don't create virtual hosts
-// $request = $_SERVER['REQUEST_URI'];
-$method = $_SERVER['REQUEST_METHOD'];
-$viewDir = '/views/';
-
-switch ($request) {
-    case '':
-    
-    case '/':
-        require __DIR__ . $viewDir . 'login.php';
-        break;
-    
-    default:
-        http_response_code(404);
-        require __DIR__ . $viewDir . '404.php';
-        break;
-}
+header('Location: login.php');
+die();
